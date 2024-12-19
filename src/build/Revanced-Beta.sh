e4f836690332c1dcefeecc6a845061b2b2e38386
f97 <312022151125@ued.udn.vn>
2024-12-19 17:05:37 +0700

    Update Revanced-Beta.sh

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -41,7 +41,7 @@ revanced_dl(){
 	# Patch Facebook:
 	# Arm64-v8a
 	get_patches_key "facebook"
-	version="494.0.0.55.73"
+	version="493.0.0.72.158"
 	get_apk "com.facebook.katana" "facebook-arm64-v8a-beta" "facebook" "facebook-2/facebook/facebook" "arm64-v8a" "nodpi" "Android 9.0+"
 	patch "facebook-arm64-v8a-beta" "revanced"
 }
