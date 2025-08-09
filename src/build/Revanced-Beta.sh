10e4555165d8c64baa008e45058c617af99178a8
f97 <f97.xyz@gmail.com>
2025-08-09 21:24:54 +0700

    Update Revanced-Beta.sh

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -41,7 +41,7 @@ revanced_dl(){
 	# Patch Facebook:
 	# Arm64-v8a
 	get_patches_key "facebook"
-	version="525.0.0.53.51" #Force this version because only patch in this version
+	version="526.0.0.0.63" #Force this version because only patch in this version
 	get_apk "com.facebook.katana" "facebook-arm64-v8a-beta" "facebook" "facebook-2/facebook/facebook" "arm64-v8a" "nodpi" "Android 11+"
 	patch "facebook-arm64-v8a-beta" "revanced"
 }
